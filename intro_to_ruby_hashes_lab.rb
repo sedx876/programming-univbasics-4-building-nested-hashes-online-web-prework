9272b3824657db6987c6f6545d94f2e3b1daade4
Sharon Watkins <swatkins876@gmail.com>
2019-08-06 15:41:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,6 +14,11 @@ end
 
 def monopoly_with_fourth_tier
    monopoly = {:railroads => {:pieces => "4",
-                             :rent_in_dollars =>
+                             :rent_in_dollars => {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    }
   }}
 end
